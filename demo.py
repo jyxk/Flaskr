02e625685d4028d52d22337187ec059f8218767f
杨威利 <jyxk@Magi.localdomain>
2018-09-29 22:48:15 +0800

    first time

---
+++ demo.py
@@ -0,0 +1,35 @@
+from flask import Flask, url_for
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s' % subpath
